45fd6d3c9e3392fc424492f26cd0421745a53051
zarazinsfuss <zarazinsfuss@protonmail.com>
2023-10-21 10:57:18 +0200

    first commit

--- scrapit.py
+++ scrapit.py
@@ -2,7 +2,16 @@ import os
 import sys
 import requests
 from bs4 import BeautifulSoup
-from git import Repo
+from urllib.parse import urljoin
+
+def download_asset(base_url, asset_url, output_folder):
+    """Download an asset and return its local path."""
+    asset_response = requests.get(urljoin(base_url, asset_url))
+    asset_name = os.path.basename(asset_url)
+    asset_path = os.path.join(output_folder, asset_name)
+    with open(asset_path, 'wb') as asset_file:
+        asset_file.write(asset_response.content)
+    return asset_name
 
 def download_content(url):
     response = requests.get(url)
@@ -12,33 +21,31 @@ def download_content(url):
     output_dir = "output"
     os.makedirs(output_dir, exist_ok=True)
 
-    # Save the main HTML
-    html_path = os.path.join(output_dir, "page.html")
-    with open(html_path, "w", encoding="utf-8") as f:
-        f.write(soup.prettify())
+    # Download and adjust CSS links
+    for link_tag in soup.find_all("link", rel="stylesheet"):
+        css_url = link_tag["href"]
+        if css_url.startswith(("http:", "https:", "//")):  # Avoid base64 or data URLs
+            local_css_name = download_asset(url, css_url, output_dir)
+            link_tag["href"] = local_css_name
 
-    # Download images
-    img_folder = os.path.join(output_dir, "images")
-    os.makedirs(img_folder, exist_ok=True)
+    # Download and adjust JS links
+    for script_tag in soup.find_all("script"):
+        js_url = script_tag.get("src")
+        if js_url and js_url.startswith(("http:", "https:", "//")):  # Avoid inline JS
+            local_js_name = download_asset(url, js_url, output_dir)
+            script_tag["src"] = local_js_name
+
+    # Download images and adjust their sources
     for img_tag in soup.find_all("img"):
         img_url = img_tag["src"]
-        img_name = os.path.basename(img_url)
-        img_path = os.path.join(img_folder, img_name)
-        with open(img_path, "wb") as img_file:
-            img_file.write(requests.get(img_url).content)
+        if img_url.startswith(("http:", "https:", "//")):  # Avoid base64 or data URLs
+            local_img_name = download_asset(url, img_url, os.path.join(output_dir, "images"))
+            img_tag["src"] = os.path.join("images", local_img_name)
 
-def upload_to_git():
-    REPO_DIR = os.getcwd()  # Assuming the script is run from the repo directory
-    repo = Repo.init(REPO_DIR)
-
-    # Add all files in the output directory to the repo
-    repo.git.add("output/*")
-
-    # Commit the changes
-    repo.git.commit("-m", "Add scraped content")
-
-    # Push to remote (assuming you've set up a remote named 'origin')
-    repo.git.push("origin", "master")
+    # Save the adjusted HTML
+    html_path = os.path.join(output_dir, "page.html")
+    with open(html_path, "w", encoding="utf-8") as f:
+        f.write(str(soup))
 
 def print_help():
     help_message = """
@@ -49,7 +56,6 @@ Arguments:
 
 Description:
     This script scrapes the content of the provided URL and saves it in the 'output/' directory.
-    It then commits and pushes the changes to a Git repository.
     """
     print(help_message)
 
@@ -60,5 +66,4 @@ if __name__ == "__main__":
 
     target_url = sys.argv[1]
     download_content(target_url)
-    upload_to_git()
 
